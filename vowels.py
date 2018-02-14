eadeb6250c1ef5deb6144e5093bd8b70da789e1f
Pavithralakshmi <36395725+Pavithralakshmi@users.noreply.github.com>
2018-02-14 16:00:54 +0530

    vowels.py

--- vowels.py
+++ vowels.py
@@ -2,5 +2,7 @@ vowels=['a','e','i','o','u']
 a=raw_input("enter your letter")
 if a in vowels:
     print("its vowel",a)
+elif a>0:
+    print("please enter valid letters")
 else:
     print("its constant",a)
