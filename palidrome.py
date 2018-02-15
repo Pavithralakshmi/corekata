259db241c43d13629cc19508325de355ce115581
Pavithralakshmi <36395725+Pavithralakshmi@users.noreply.github.com>
2018-02-15 14:50:41 +0530

    palindrome.py

--- palidrome.py
+++ palidrome.py
@@ -1,11 +1,11 @@
-n=int(input("Enter number:"))
-temp=n
-rev=0
-while(n>0):
+num=int(input("Enter number:"))
+temp=num
+re=0
+while(num>0):
     dig=n%10
-    rev=rev*10+dig
-    n=n//10
-if(temp==rev):
+    re=re*10+dig
+    num=num//10
+if(temp==re):
     print("The number is a palindrome!")
 else:
     print("The number isn't a palindrome!")
