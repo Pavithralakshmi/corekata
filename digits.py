c19aaf7ee6c6aa9959a3020b2d9857464d779a2b
Pavithralakshmi <36395725+Pavithralakshmi@users.noreply.github.com>
2018-02-15 12:24:57 +0530

    digits.py

--- digits.py
+++ digits.py
@@ -1,9 +1,9 @@
-n=int(input("Enter number:"))
-count=0
-if count>=0:
-    while(n>0):
+n=int(raw_input("enter the number of values"))
+if n>0:
+    count=0
+    while n>0:
         count=count+1
         n=n//10
-        print("The number of digits in the number are:",count)
+    print(count)
 else:
     print("invalid")
