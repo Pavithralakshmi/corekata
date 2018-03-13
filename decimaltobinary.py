6e6f5fa9fcfd79b7468729f3c1f1c2d79b4a15e3
Pavithralakshmi <36395725+Pavithralakshmi@users.noreply.github.com>
2018-03-13 17:36:19 +0530

    decimaltobinary.py

---
+++ decimaltobinary.py
@@ -0,0 +1,13 @@
+def bTod(binary):
+    binary1 = binary
+    decimal, i, n = 0, 0, 0
+    while(binary != 0):
+        dec = binary % 10
+        decimal = decimal + dec * pow(2, i)
+        binary = binary//10
+        i += 1
+    print(decimal)    
+if __name__ == '__main__':
+    bTod(100)
+    bTod(101)
+    bTod(1001)
