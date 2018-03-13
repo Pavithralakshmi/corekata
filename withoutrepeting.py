9f9cb50c6f6aff6ed46c20c392d55d4c982bbc1d
Pavithralakshmi <36395725+Pavithralakshmi@users.noreply.github.com>
2018-03-14 00:52:27 +0530

    withoutrepeting.py

--- withoutrepeting.py
+++ withoutrepeting.py
@@ -1,25 +1,28 @@
-NO_OF_CHARS = 256
-def longestUniqueSubsttr(string):
+CHARS = 256
+def rr(string):
 	n = len(string)
-	cur_len = 1	 
-	max_len = 1	 
-	prev_index = 0 
+	cur = 1	 
+	max= 1	 
+	index = 0 
 	i = 0
-	visited = [-1] * NO_OF_CHARS
+	visited = [-1] * CHARS
 	visited[ord(string[0])] = 0
 	for i in xrange(1,n):
-		prev_index = visited[ord(string[i])]
-		if prev_index == -1 or (i - cur_len > prev_index):
-			cur_len+=1
+		index = visited[ord(string[i])]
+		if index == -1 or (i - cur > index):
+			cur+=1
 		else:
-			if cur_len > max_len:
-				max_len = cur_len
-			cur_len = i - prev_index
+			if cur > max:
+				max = cur
+			cur = i - index
 		visited[ord(string[i])] = i
-	if cur_len > max_len:
-		max_len = cur_len
-	return max_len
+	if cur > max:
+		max = cur
+	return max
 string = "my name is billa"
 print "The input string is " + string
-length = longestUniqueSubsttr(string)
+length = rr(string)
 print ("The length of the longest non-repeating character" +" substring is " + str(length))
+
+
+
