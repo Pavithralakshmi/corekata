52baf38bd854550dd6ed583149959e6efbae5044
Pavithralakshmi <36395725+Pavithralakshmi@users.noreply.github.com>
2018-03-14 17:01:39 +0530

    Update digit.py

--- digit.py
+++ digit.py
@@ -1,6 +1,6 @@
-count = 0
-number = int(input("Enter a number "))
-while (number > 0):
-  number = number//10
-  count = count + 1
-print ("Total number of digits : ",count)
+de = 0
+n = int(input("Enter a number "))
+while (n > 0):
+  n = n//10
+  de = de + 1
+print ("Total number of digits : ",de)
