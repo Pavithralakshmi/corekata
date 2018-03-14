332e584330c92a55f4b030a51692fab9d6342f95
Pavithralakshmi <36395725+Pavithralakshmi@users.noreply.github.com>
2018-03-14 18:03:19 +0530

    Update uop.py

--- uop.py
+++ uop.py
@@ -1,4 +1,4 @@
-s= "this should be uppercase!"
-print(s.upper())
-ser = raw_input("enter")
-print(ser.upper())
+s3= "this should be uppercase!"
+print(s3.upper())
+ser3 = raw_input("enter")
+print(ser3.upper())
