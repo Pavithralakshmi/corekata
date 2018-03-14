679ab9ec4b31fc40b666181d95252c5dd779a09a
Pavithralakshmi <36395725+Pavithralakshmi@users.noreply.github.com>
2018-03-14 17:54:21 +0530

    hh.py

---
+++ hh.py
@@ -0,0 +1,14 @@
+t = [11567, 675, 26, 37, 1, 2, 4, 5]
+
+u = int(input("Enter number to search: "))
+
+found = False
+
+for i in range(len(t)):
+ if(t[i] == u:
+  found = True
+  print("%d found at %dth position"%(ui))
+  break
+ 
+if(found == False):
+ print("%d is not in list"%u
