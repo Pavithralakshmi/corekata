42025c48f97ee52346c1f4981350071d072db5f1
Pavithralakshmi <36395725+Pavithralakshmi@users.noreply.github.com>
2018-03-17 11:59:05 +0530

    asciiletter.py

---
+++ asciiletter.py
@@ -0,0 +1,7 @@
+print("*****ASCII CODE******")
+y=0
+while y==0:
+  QQ=int(input("enter your number"))
+  print("The letter of ASCII is ",chr(QQ))
+  y=int(input("if u select 0 it tobe contine else select 1 to stop"))
+  
