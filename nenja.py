0b12487d6737909e8aa1f1337b259148ff726b7b
Pavithralakshmi <36395725+Pavithralakshmi@users.noreply.github.com>
2018-03-17 10:10:44 +0530

    nenja.py

---
+++ nenja.py
@@ -0,0 +1,8 @@
+print("***nenja's and kabali's clan ***")
+print("enter nenja's team members counts")
+nenjasteam=int(input("enter kabalish's team members"))
+print("enter kabalish's team members counts")
+kabalishsteam=int(input("enter kabalish's team members"))
+print("difference between two teams")
+dif=nenjasteam-kabalishsteam
+print("diference between two teams:",dif)
