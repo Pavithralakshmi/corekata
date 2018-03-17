9bd12c8f003947841fedf0f6aeb6e9b126e37a05
Pavithralakshmi <36395725+Pavithralakshmi@users.noreply.github.com>
2018-03-17 11:50:09 +0530

    asciiword.py

---
+++ asciiword.py
@@ -0,0 +1,7 @@
+print("*****ASCII CODE******")
+y=0
+while y==0:
+  QQ=input("enter your letter")
+  print("The value of ASCII code",ord(QQ))
+  y=int(input("if u select 0 it tobe contine else select 1 to stop"))
+  
