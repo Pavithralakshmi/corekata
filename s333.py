aeeccb3e0259b0879b8ed93d60e34701f172cbf2
Pavithralakshmi <36395725+Pavithralakshmi@users.noreply.github.com>
2018-03-19 17:38:59 +0530

    s333.py

---
+++ s333.py
@@ -0,0 +1,7 @@
+a11 = input('Enter value of a11: ')
+b11 = input('Enter value of b11: ')
+temp = a11
+a11 = b11
+b11 = temp
+print('The value of x after swapping: {}'.format(a11))
+print('The value of y after swapping: {}'.format(b11))
