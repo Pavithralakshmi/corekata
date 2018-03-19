51a03932390ebb76f6a866c797eb9349fa39804d
Pavithralakshmi <36395725+Pavithralakshmi@users.noreply.github.com>
2018-03-19 16:49:52 +0530

    Update s2.py

--- s2.py
+++ s2.py
@@ -1,6 +1,6 @@
-lst1= []
-num1= int(input('How many numbers: '))
-for n in range(num1):
+lst12= []
+n1= int(input('How many numbers: '))
+for n in range(n1):
     numbers = int(input('Enter number '))
     lst.append(numbers)
-print("Maximum element in the list is :", max(lst1), "\nMinimum element in the list is :", min(lst1))
+print("Maximum element in the list is :", max(lst12), "\nMinimum element in the list is :", min(lst12))
