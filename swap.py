79bc4c70890b49f014df2e88dc6824752bf8b9ed
Pavithralakshmi <36395725+Pavithralakshmi@users.noreply.github.com>
2018-03-19 16:44:20 +0530

    swap.py

---
+++ swap.py
@@ -0,0 +1,7 @@
+a1 = input('Enter value of a1: ')
+b1 = input('Enter value of b1: ')
+temp = a1
+a1 = b1
+b1 = temp
+print('The value of x after swapping: {}'.format(a1))
+print('The value of y after swapping: {}'.format(b1))
