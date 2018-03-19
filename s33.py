2647f1bab04f5c0c8f482179dbf6e992a57f1638
Pavithralakshmi <36395725+Pavithralakshmi@users.noreply.github.com>
2018-03-19 17:36:37 +0530

    s33.py

---
+++ s33.py
@@ -0,0 +1,6 @@
+lst132= []
+n13= int(input('How many numbers: '))
+for n in range(n13):
+    numbers = int(input('Enter number '))
+    lst.append(numbers)
+print("Maximum element in the list is :", max(lst132), "\nMinimum element in the list is :", min(lst132))
