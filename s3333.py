b98fd0a196ba48ca18747141a05cede1d0bf558e
Pavithralakshmi <36395725+Pavithralakshmi@users.noreply.github.com>
2018-03-19 17:41:15 +0530

    s3333.py

---
+++ s3333.py
@@ -0,0 +1,8 @@
+aa11=int(input())
+ba11=int(input())
+aa11 = aa11 ^ ba11
+ba11 = aa11 ^ ba11
+aa11 = aa11 ^ ba11
+print("== After Swapping ==")
+print("First value is ",aa11)
+print("Second value is ",ba11)
