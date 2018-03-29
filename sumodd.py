12260b9126b34dcc31cc2634984451557fea88e2
Pavithralakshmi <36395725+Pavithralakshmi@users.noreply.github.com>
2018-03-29 20:13:49 +0530

    sumodd.py

---
+++ sumodd.py
@@ -0,0 +1,13 @@
+print("cheack the sum of value is odd or even");
+y=0
+while y==0:
+  first=int(input("enter ur first input"))
+  second=int(input("enter second input"))
+  print("sum is",first+second)
+  sum= first+second
+  print("condition")
+  if sum%2==0:
+    print("it is even")
+  else:
+    print("it is odd")
+  y=int(input("ënter 0 to continue else press 1"))
