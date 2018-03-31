2204bc607cea7032ff2afadb6cfc967adc882be4
Pavithralakshmi <36395725+Pavithralakshmi@users.noreply.github.com>
2018-03-31 21:44:22 +0530

    poer.py

---
+++ poer.py
@@ -0,0 +1,9 @@
+print(" power of input")
+y=0
+while y==0:
+  number1 = int(input('Enter First number : '))
+  number2 = int(input('Enter Second number : '))
+  o=number1**number2
+  print(o)
+  y=int(input("0 to continue"))
+    
