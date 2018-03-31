e1e2b3a6cd919aa789f6e7d831854b80a04ccb41
Pavithralakshmi <36395725+Pavithralakshmi@users.noreply.github.com>
2018-03-31 21:30:41 +0530

    incer1.py

---
+++ incer1.py
@@ -0,0 +1,9 @@
+print(" increase the one value")
+y=0
+u=0
+while y==0:
+  n=int(input("Enter number:"))
+  u=n+1
+  print("The number of digits in the number are:",u)
+  y=int(input("0 to continue"))
+    
