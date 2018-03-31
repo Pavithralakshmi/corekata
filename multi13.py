e5c007038fb0b041e7c131ffaacfbb22c476acb1
Pavithralakshmi <36395725+Pavithralakshmi@users.noreply.github.com>
2018-03-31 20:21:01 +0530

    multi13.py

---
+++ multi13.py
@@ -0,0 +1,10 @@
+print("the given input is multiple by 13 or not")
+y=0
+while y==0:
+  n=int(input("ënter your value"))
+  if n%13==0:
+    print(" yes,this number is multiple by 13")
+  else:
+    print( "no,this number is  not multiple by 13")
+  y=int(input("press 0 to continue"))
+      
