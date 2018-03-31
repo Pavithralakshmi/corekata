5ec06596ee0b5d6ee08c250d9a862d9c618a7ceb
Pavithralakshmi <36395725+Pavithralakshmi@users.noreply.github.com>
2018-03-31 21:50:50 +0530

    areasurface.py

---
+++ areasurface.py
@@ -0,0 +1,10 @@
+print(" area of surface")
+y=0
+while y==0:
+  l = int(input('Enter length value : '))
+  b = int(input('Enter breath value : '))
+  c = int(input('enter coubid value: '))
+  o=l*b*c
+  print(o)
+  y=int(input("0 to continue"))
+    
