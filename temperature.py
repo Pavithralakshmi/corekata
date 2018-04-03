78ad38b2f539621cd6be63bad7304e0e9a66e381
Pavithralakshmi <36395725+Pavithralakshmi@users.noreply.github.com>
2018-04-03 11:59:12 +0530

    temperature.py

---
+++ temperature.py
@@ -0,0 +1,9 @@
+print("Temperature")
+y=0
+while y==0:
+  def Celsius_to_Kelvin(C):
+  	return (C + 273.15)
+  C = int(input("enter celsius value"))
+  print("Temperature in Kelvin ( K ) = ", Celsius_to_Kelvin(C))
+  y=int(input("press 0 to continue"))
+    
