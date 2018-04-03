c1bc9450f92ba68584ad7c8eabf96831e645dd9c
Pavithralakshmi <36395725+Pavithralakshmi@users.noreply.github.com>
2018-04-03 11:45:13 +0530

    simpleinterest.py

---
+++ simpleinterest.py
@@ -0,0 +1,11 @@
+print("simple interest")
+y=0
+while y==0:
+  p=int(input("enter p value"))
+  n=int(input("enter n value"))
+  r=int(input("enter r value"))
+  yyy=p*n*r
+  simple_interest = yyy/100
+  print( "simple interest is:",simple_interest)
+  y=int(input("press 0 to continue"))
+  
