9257cc017facb79b9d80f19e20ec7c68d15af0d6
Pavithralakshmi <36395725+Pavithralakshmi@users.noreply.github.com>
2018-04-07 16:06:35 +0530

    minarm.py

---
+++ minarm.py
@@ -0,0 +1,6 @@
+rt= []
+n13= int(input('How many numbers: '))
+for n in range(n13):
+    numbers = int(input('Enter number '))
+    rt.append(numbers)
+print("Minimum element in the list is :", min(rt))
