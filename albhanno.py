2641951b741f1cf43ce50c943686454202df179c
Pavithralakshmi <36395725+Pavithralakshmi@users.noreply.github.com>
2018-04-07 15:18:09 +0530

    albhanno.py

---
+++ albhanno.py
@@ -0,0 +1,3 @@
+letter = ['','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+br=int(input("enter ur query number:"))
+print letter[br]
