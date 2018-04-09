35426bb564e57eac92ced11531dfd69ddab10497
Pavithralakshmi <36395725+Pavithralakshmi@users.noreply.github.com>
2018-04-09 20:35:52 +0530

    Update day.py

--- day.py
+++ day.py
@@ -1,9 +1,10 @@
-time = float(input("Input time in seconds: "))
-day = time // (24 * 3600)
-time = time % (24 * 3600)
-hour = time // 3600
-time %= 3600
-minutes = time // 60
-time %= 60
-seconds = time
+print("find day and minitues" )
+t = float(input("Input time in seconds: "))
+day = t // (24 * 3600)
+t = t % (24 * 3600)
+hour = t // 3600
+t %= 3600
+minutes = t // 60
+t %= 60
+seconds = t
 print("d:h:m:s-> %d:%d:%d:%d" % (day, hour, minutes, seconds))
