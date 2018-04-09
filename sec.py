81abb4a923a3aa956acc6c400345ee23b083af29
Pavithralakshmi <36395725+Pavithralakshmi@users.noreply.github.com>
2018-04-09 20:02:40 +0530

    sec.py

---
+++ sec.py
@@ -0,0 +1,8 @@
+print("calculate amounts of seconds ")
+days =int(input("Inputdays: ")) * 3600* 24
+
+hours = int(input("Input hours: ")) * 3600
+minutes = int(input("Input minutes: ")) * 60
+seconds = int(input("Input seconds: "))
+time = days + hours + minutes + seconds
+print("The amounts of seconds ", time)
