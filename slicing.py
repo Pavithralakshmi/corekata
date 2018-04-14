097aca8b81672633365d242194ee2f989b3eca7d
Pavithralakshmi <36395725+Pavithralakshmi@users.noreply.github.com>
2018-04-14 20:20:11 +0530

    slicing.py

---
+++ slicing.py
@@ -0,0 +1,9 @@
+print("print str with omit first letter")
+y=0
+while y==0:
+  print("Enter the string:")
+  n=str(input("Enter something:"))
+  d=len(n)
+  s=n[1:d]
+  print(" output of this prg is:",s)
+  y=int(input("press 0 to continue"))
