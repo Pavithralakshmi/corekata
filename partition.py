cbd89e93861f801feb7e34609dfb61918bc8c4fd
Pavithralakshmi <36395725+Pavithralakshmi@users.noreply.github.com>
2018-04-15 21:18:34 +0530

    partition.py

---
+++ partition.py
@@ -0,0 +1,11 @@
+print("partition")
+y=0
+while y==0:
+  n=int(input("enter n"))
+  a=int(input("enter a"))
+  b=int(input("enter b"))
+  if n%2==0:
+      print("yes")
+  else:
+      print("no")
+  y=int(input("press 0 to continue"))
