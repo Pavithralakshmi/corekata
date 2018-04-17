7ce490df668e266ccd6856aec202886758840748
Pavithralakshmi <36395725+Pavithralakshmi@users.noreply.github.com>
2018-04-17 22:48:45 +0530

    Update a2.py

--- a2.py
+++ a2.py
@@ -1,13 +1,13 @@
-def max( a, size):
-    for i in range(size):
+def large( a, n):
+    for i in range(n):
         if a[abs(a[i])-1] > 0:
             a[abs(a[i])-1] = -a[abs(a[i])-1]
         else:
             print("repeating number",abs(a[i]))
              
-    for i in range(size):
+    for i in range(n):
         if a[i]>0:
             print(" missing number ",i+1)
-a = [6, 2, 3, 4, 4, 5, 2]
+a = [6, 2,46, 3, 44, 6, 5, 2]
 n = len(a)
-printmax(a,n)
+print(large(a,n))
