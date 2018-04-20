7a440650de42dec253b2f91d6d357e57c013cc3d
Pavithralakshmi <36395725+Pavithralakshmi@users.noreply.github.com>
2018-04-20 20:31:39 +0530

    Update asen.py

--- asen.py
+++ asen.py
@@ -1,11 +1,11 @@
 y=0
 while y==0:
-  n=int(input("enter total number"))
-  b=[]
+  n=int(input("enter your limits"))
+  r=[]
   for i in range(1,n+1):
       a=input("enter a number")
-      b.append(a)
+      r.append(a)
   for i in range(1,n+1):
-      print(max(b))
-      b.remove(max(b))
+      print(max(r))
+     r.remove(max(r))
   y=int(input("press 0 to continue"))
