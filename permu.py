5dee40d61a6624081c6dcc50476959837a42973c
Pavithralakshmi <36395725+Pavithralakshmi@users.noreply.github.com>
2018-04-21 23:30:56 +0530

    permu.py

--- permu.py
+++ permu.py
@@ -1,13 +1,13 @@
 a=int(input("Enter first number:"))
 b=int(input("Enter second number:"))
 c=int(input("Enter third number:"))
-d=[]
-d.append(a)
-d.append(b)
-d.append(c)
+s=[]
+s.append(a)
+s.append(b)
+s.append(c)
 for i in range(0,3):
     for j in range(0,3):
         for k in range(0,3):
             if(i!=j&j!=k&k!=i):
-                print(d[i],d[j],d[k])
+                print(s[i],d[j],d[k])
                 break
