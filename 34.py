4c8f23f23077de4580f948d96983495a730a9dae
Pavithralakshmi <36395725+Pavithralakshmi@users.noreply.github.com>
2018-04-22 00:01:06 +0530

    34.py

---
+++ 34.py
@@ -0,0 +1,13 @@
+q=[]
+i=0
+n=(int(input("enter the no of elements")))
+for i in range(i,n):
+  b=input("enter number")
+  q.append(b)
+print(q)
+i=0
+for i in range(i,n):
+    if(q[i]==i):
+        print(q[i])
+   
+print('-1')
