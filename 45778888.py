cc65f1f959bc6d21338a152a40a971a171c1a1ad
Pavithralakshmi <36395725+Pavithralakshmi@users.noreply.github.com>
2018-04-23 21:07:06 +0530

    45778888.py

---
+++ 45778888.py
@@ -0,0 +1,11 @@
+y="r"
+while (y!='x'):
+  print("Enter'x' for exit")
+  str1=str(input("Enterthe str1"))
+  str2=str(input("Enterthe str2"))
+  if (str1=="x"):
+    break
+  else:
+    temp=str1
+    print(str2,str1)
+  y=str(input("enter r to continue"))
