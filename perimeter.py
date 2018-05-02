cccc7b90e08e305da9970dd314988b51007a0cf9
Pavithralakshmi <36395725+Pavithralakshmi@users.noreply.github.com>
2018-05-02 22:41:20 +0530

    perimeter.py

---
+++ perimeter.py
@@ -0,0 +1,14 @@
+a=int(input("enter a number"))
+b=int(input("enter b number"))
+if(a>b):
+  if(b+b<a):
+    print("no")
+  else:
+    print("yes")
+elif(a<b):
+  if(a+a<b):
+    print("no")
+  else:
+    print("yes")
+else:
+    print("no")
