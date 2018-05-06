abd3460ec6bb3c2cf6a493eb2275b397adf11f76
Pavithralakshmi <36395725+Pavithralakshmi@users.noreply.github.com>
2018-05-07 01:58:44 +0530

    wq.py

---
+++ wq.py
@@ -0,0 +1,13 @@
+ 
+l = []
+y=[]
+n=int(input("enter:"))
+for i in range(1,n+1):
+  b=int(input("enter"))
+  l.append(b)
+for j in range(1,n+1):
+  c=int(input("enter"))
+  y.append(c)
+print(max(l))
+print(max(y))
+
